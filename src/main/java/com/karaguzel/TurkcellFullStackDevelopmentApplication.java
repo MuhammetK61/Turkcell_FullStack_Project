fbd12d0834d7a6b0005f98cfc9872ee2e6df56fe
Muhammet Ali Karagüzel <muhammetali.karaguzel61@gmail.com>
2022-08-13 23:56:15 +0300

    project

---
+++ src/main/java/com/karaguzel/TurkcellFullStackDevelopmentApplication.java
@@ -0,0 +1,28 @@
+package com.karaguzel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+//@SpringBootApplication
+//// websecurity exclude
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+//Auditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class TurkcellFullStackDevelopmentApplication {
+    //PSVM
+    public static void main(String[] args) {
+        //devtool enabled tru
+        System.setProperty("spring.devtools.restart.enabled", "true");
+
+        //JOptionalPane calisamasi icin
+        System.setProperty("java.awt.headless", "false"); // Disables headless
+
+        //Main Running
+        SpringApplication.run(TurkcellFullStackDevelopmentApplication.class, args);
+    }
+    // @Bean yazma yeri
+}
---
+++ src/test/java/com/karaguzel/TurkcellFullStackDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karaguzel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurkcellFullStackDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
